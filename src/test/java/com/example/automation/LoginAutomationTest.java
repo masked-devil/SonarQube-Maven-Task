a2cfde9ee180fefed5ff2c8b149622b9a713d01e
Krishna Jarhad <112654773+masked-devil@users.noreply.github.com>
2025-01-03 17:29:22 +0530

    Update LoginAutomationTest.java

--- src/test/java/com/example/automation/LoginAutomationTest.java
+++ src/test/java/com/example/automation/LoginAutomationTest.java
@@ -1,1 +1,47 @@
+package com.example.automation;
 
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class LoginAutomationTest {
+
+    @Test
+    public void testLogin() {
+        
+        System.setProperty("webdriver.chrome.driver", "C://Program Files//chromedriver//chromedriver.exe");
+
+        
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless"); 
+        
+        
+
+        WebDriver driver = new ChromeDriver(options);
+
+        try {
+            
+            LoginPage loginPage = new LoginPage(driver);
+
+            
+            loginPage.openLoginPage("https://the-internet.herokuapp.com/login");
+
+            
+            loginPage.setUsername("tomsmith");
+            loginPage.setPassword("SuperSecretPassword!");
+            loginPage.clickLoginButton();
+
+            
+            String successMessage = loginPage.getSuccessMessage();
+            assertTrue(successMessage.contains("You logged into a secure area!"),
+                    "Login  successful ");
+
+        } finally {
+            
+            driver.quit();
+        }
+    }
+}
